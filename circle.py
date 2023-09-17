32252d07e27bfee790e4d42dcb0df42e0bb7bb95
personnumber3377 <sontapaa.jokulainen@gmail.com>
2023-09-17 18:05:39 +0300

    Added stuff.

--- circle.py
+++ circle.py
@@ -37,6 +37,7 @@ class Circle:
 		self.radius = radius
 		self.position = position
 		self.velocity = velocity
+		self.checked = False
 
 	def get_mag_vel(self):
 
@@ -65,8 +66,50 @@ class Circle:
 			ret = (3*math.pi/2)+(math.pi/2+ret)
 		return ret
 
+	def get_angle_vec(self, vector: np.array):
+
+		x_vel = vector[0]
+		y_vel = vector[1]
+
+		if x_vel == 0.0:
+			if y > 0:
+				return math.pi/2
+			elif y == 0:
+				return 0.0
+			else:
+				return (3*math.pi)/2
+
+		ret = math.atan(float(y/x))
+		
+		if (x < 0 and y < 0) or x < 0:
+			ret = math.pi + ret
+		elif y < 0:
+			ret = (3*math.pi/2)+(math.pi/2+ret)
+
+		return ret
+
 
 	def update_velocity(self,othercircle: Circle): # Update the velocity of this ball with the other ball.
+		
+		if self.checked:
+			return
 
+		x_vel = self.velocity[0]
+		y_vel = self.velocity[1]
 
+		# Now we use the equations as described in the wikipedia article.
+		v_1 = self.get_mag_vel()
+		v_2 = othercircle.get_mag_vel()
 
+		theta_1 = self.get_mov_angle()
+		theta_2 = othercircle.get_mov_angle()
+
+		vector_to_other = -1*self.position+othercircle.position # The vector from A to B is  (-1 * A) + B
+		phi = self.get_angle_vec(vector_to_other)
+		m_1 = self.mass
+		m_2 = othercircle.mass
+
+		new_x_vel = (((vel_mag)*math.cos(theta_1 - phi)*(m_1 - m_2) + (2*m_2*v_2*math.cos(theta_2 - phi)))/(m_1+m_2))*math.cos(phi) + v_1*math.sin(theta_1 - phi)*math.cos(phi+math.pi/2)
+		new_y_vel = (((vel_mag)*math.cos(theta_1 - phi)*(m_1 - m_2) + (2*m_2*v_2*math.cos(theta_2 - phi)))/(m_1+m_2))*math.sin(phi) + v_1*math.sin(theta_1 - phi)*math.sin(phi+math.pi/2)
+
+		return
